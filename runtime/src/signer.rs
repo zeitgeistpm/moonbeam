9b3d893465d2aedae04be0de1f2b44bea6cc4fa2
crystalin <alan.sapede@gmail.com>
2020-08-16 14:56:38 -0400

    Clean Ethereum Signature

--- runtime/src/signer.rs
+++ runtime/src/signer.rs
@@ -7,51 +7,42 @@ use sp_std::convert::TryFrom;
 
 #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Eq, PartialEq, Clone, Encode, Decode, sp_core::RuntimeDebug)]
-pub enum MultiSignature {
-	/// An ECDSA/SECP256k1 signature.
-	Ecdsa(ecdsa::Signature),
-}
+pub struct EthereumSignature(ecdsa::Signature);
 
-impl From<ecdsa::Signature> for MultiSignature {
+impl From<ecdsa::Signature> for EthereumSignature {
 	fn from(x: ecdsa::Signature) -> Self {
 		log::info!("From<ecdsa::Signature> ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		MultiSignature::Ecdsa(x)
+		EthereumSignature(x)
 	}
 }
 
-impl TryFrom<MultiSignature> for ecdsa::Signature {
-	type Error = ();
-	fn try_from(m: MultiSignature) -> Result<Self, Self::Error> {
-		log::info!("TryFrom<MultiSignature> ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		if let MultiSignature::Ecdsa(x) = m { Ok(x) } else { Err(()) }
-	}
-}
+// impl TryFrom<EthereumSignature> for ecdsa::Signature {
+// 	type Error = ();
+// 	fn try_from(es: EthereumSignature) -> Result<Self, Self::Error> {
+// 		Ok(es)
+// 	}
+// }
 
-impl Default for MultiSignature {
-	fn default() -> Self {
-		log::info!("Default MultiSignature ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		MultiSignature::Ecdsa(Default::default())
-	}
-}
+// impl Default for EthereumSignature {
+// 	fn default() -> Self {
+// 		EthereumSignature(Default::default())
+// 	}
+// }
 
 /// Public key for any known crypto algorithm.
 #[derive(Eq, PartialEq, Ord, PartialOrd, Clone, Encode, Decode, sp_core::RuntimeDebug)]
 #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
-pub enum MultiSigner {
-	/// An SECP256k1/ECDSA identity (actually, the Blake2 hash of the compressed pub key).
-	Ecdsa(ecdsa::Public),
-}
+pub struct EthereumSigner (ecdsa::Public);
 
-impl Default for MultiSigner {
-	fn default() -> Self {
-		log::info!("Default for MultiSigner ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		MultiSigner::Ecdsa(Default::default())
-	}
-}
+// impl Default for EthereumSigner {
+// 	fn default() -> Self {
+// 		EthereumSigner(Default::default())
+// 	}
+// }
 
 /// NOTE: This implementations is required by `SimpleAddressDeterminer`,
 /// we convert the hash into some AccountId, it's fine to use any scheme.
-impl<T: Into<H256> + sp_std::fmt::Debug> sp_core::crypto::UncheckedFrom<T> for MultiSigner {
+impl<T: Into<H256> + sp_std::fmt::Debug> sp_core::crypto::UncheckedFrom<T> for EthereumSigner {
 	fn unchecked_from(x: T) -> Self {
 		log::info!("UncheckedFrom: {:?}", x);
 		let mut value = [0u8; 33];
@@ -61,68 +52,48 @@ impl<T: Into<H256> + sp_std::fmt::Debug> sp_core::crypto::UncheckedFrom<T> for M
 	}
 }
 
-impl AsRef<[u8]> for MultiSigner {
-	fn as_ref(&self) -> &[u8] {
-		match *self {
-			MultiSigner::Ecdsa(ref who) => who.as_ref(),
-		}
-	}
-}
-
-impl sp_runtime::traits::IdentifyAccount for MultiSigner {
+impl sp_runtime::traits::IdentifyAccount for EthereumSigner {
 	type AccountId = super::account::AccountId20;
 	fn into_account(self) -> super::account::AccountId20 {
-		match self {
-			MultiSigner::Ecdsa(who) => {
-				log::info!("MultiSigner::Ecdsa(who) ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-				let mut value = [0u8; 20];
-				value.copy_from_slice(&sp_io::hashing::blake2_256(&who.as_ref()[..])[(32 - 20)..]);
-				value.into()
-			}
-		}
+		log::info!("EthereumSigner::into_account() ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
+		let mut value = [0u8; 20];
+		value.copy_from_slice(&sp_io::hashing::blake2_256(&self.0.as_ref()[..])[(32 - 20)..]);
+		value.into()
 	}
 }
 
-impl From<ecdsa::Public> for MultiSigner {
+impl From<ecdsa::Public> for EthereumSigner {
 	fn from(x: ecdsa::Public) -> Self {
-		log::info!("impl From<ecdsa::Public> for MultiSigner ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		MultiSigner::Ecdsa(x)
+		EthereumSigner(x)
 	}
 }
 
-impl TryFrom<MultiSigner> for ecdsa::Public {
-	type Error = ();
-	fn try_from(m: MultiSigner) -> Result<Self, Self::Error> {
-		log::info!("impl TryFrom<MultiSigner> for ecdsa::Public ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		if let MultiSigner::Ecdsa(x) = m { Ok(x) } else { Err(()) }
-	}
-}
+// impl TryFrom<EthereumSigner> for ecdsa::Public {
+// 	type Error = ();
+// 	fn try_from(m: EthereumSigner) -> Result<Self, Self::Error> {
+// 		Ok(m)
+// 	}
+// }
 
 #[cfg(feature = "std")]
-impl std::fmt::Display for MultiSigner {
+impl std::fmt::Display for EthereumSigner {
 	fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-		match *self {
-			MultiSigner::Ecdsa(ref who) => write!(fmt, "ecdsa: {}", who),
-		}
+		write!(fmt, "ethereum signature: {}", self)
 	}
 }
 
-impl sp_runtime::traits::Verify for MultiSignature {
-	type Signer = MultiSigner;
+impl sp_runtime::traits::Verify for EthereumSignature {
+	type Signer = EthereumSigner;
 	fn verify<L: sp_runtime::traits::Lazy<[u8]>>(&self, mut msg: L, signer: &super::account::AccountId20) -> bool {
-		log::info!("impl sp_runtime::traits::Verify for MultiSignature ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
-		match (self, signer) {
-			(MultiSignature::Ecdsa(ref sig), who) => {
-				let m = sp_io::hashing::blake2_256(msg.get());
-				match sp_io::crypto::secp256k1_ecdsa_recover_compressed(sig.as_ref(), &m) {
-					Ok(pubkey) => {
-						let mut value = [0u8; 20];
-						value.copy_from_slice(&sp_io::hashing::blake2_256(pubkey.as_ref())[(32 - 20)..]);
-						&value == <dyn AsRef<[u8; 20]>>::as_ref(who)
-					},
-					_ => false,
-				}
-			}
+		log::info!("impl sp_runtime::traits::Verify for EthereumSignature ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ");
+		let m = sp_io::hashing::blake2_256(msg.get());
+		match sp_io::crypto::secp256k1_ecdsa_recover_compressed(self.0.as_ref(), &m) {
+			Ok(pubkey) => {
+				let mut value = [0u8; 20];
+				value.copy_from_slice(&sp_io::hashing::blake2_256(pubkey.as_ref())[(32 - 20)..]);
+				&value == <dyn AsRef<[u8; 20]>>::as_ref(signer)
+			},
+			_ => false,
 		}
 	}
 }
