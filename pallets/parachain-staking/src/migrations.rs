8e4d233b04eaf7ed7025398364f4ab2d5c217a61
Chralt98 <chralt98@gmail.com>
2025-05-22 15:54:05 +0200

    wip

--- pallets/parachain-staking/src/migrations.rs
+++ pallets/parachain-staking/src/migrations.rs
@@ -335,16 +335,20 @@ impl<T: Config> OnRuntimeUpgrade for MigrateAtStakeAutoCompound<T> {
 		for round in 0..=current_round {
 			<AtStake<T>>::iter_key_prefix(round).for_each(move |candidate| {
 				let key = <AtStake<T>>::hashed_key_for(round.clone(), candidate.clone());
+				// a686a3043d0adcf2fa655e57bc595a78f2ea452256cacfadf13b115a94c4029c ... 000121ee5d57b8f55d8302 ... 00734d3b8ce9c2334802e70700e63984105006dfe059304ee5a84dd47593d7e493be18134892ac665e
 				if hex::encode(&key) == "a686a3043d0adcf2fa655e57bc595a78f2ea452256cacfadf13b115a94c4029c000121ee5d57b8f55d830200734d3b8ce9c2334802e70700e63984105006dfe059304ee5a84dd47593d7e493be18134892ac665e" {
-					log::info!("PRE_UPGRADE: Found FIRST key: {:?} for round: {:?}, candidate: {:?}", key, round, candidate);
+					log::info!("PRE_UPGRADE 0: Found FIRST key: {:?} for round: {:?}, candidate: {:?}", key, round, candidate);
+				}
+				if hex::encode(&key) == "a686a3043d0adcf2fa655e57bc595a78f2ea452256cacfadf13b115a94c4029ce187f8b4cc2a47951dc30300734d3b8ce9c2334802e70700e63984105006dfe059304ee5a84dd47593d7e493be18134892ac665e" {
+					log::info!("PRE_UPGRADE 1: Found LAST key: {:?} for round: {:?}, candidate: {:?}", key, round, candidate);
 				}
 				if hex::encode(&key) == "a686a3043d0adcf2fa655e57bc595a78f2ea452256cacfadf13b115a94c4029cffffd3a46cb21b071fe60300734d3b8ce9c2334802e70700e63984105006dfe059304ee5a84dd47593d7e493be18134892ac665e" {
-					log::info!("PRE_UPGRADE: Found LAST key: {:?} for round: {:?}, candidate: {:?}", key, round, candidate);
+					log::info!("PRE_UPGRADE 2: Found LAST key: {:?} for round: {:?}, candidate: {:?}", key, round, candidate);
 				}
 				let suspicious_candidate: [u8; 32] = hex_literal::hex!["02e70700e63984105006dfe059304ee5a84dd47593d7e493be18134892ac665e"].into();
 				let actual_candidate: [u8; 32] = Self::account_to_bytes(&candidate).unwrap();
 				if actual_candidate == suspicious_candidate {
-					log::info!("PRE_UPGRADE: Found candidate: {:?} for key: {:?}", candidate.clone(), hex::encode(&key));
+					// log::info!("PRE_UPGRADE: Found candidate: {:?} for key: {:?}", candidate.clone(), hex::encode(&key));
 				}
 			});
 		}
